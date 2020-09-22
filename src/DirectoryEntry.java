00d99f777560a66a81e0ab9ff294c518b9f1faff
drbhrigucelly <33190843+drbhrigucelly@users.noreply.github.com>
2020-09-21 23:58:44 -0700

    Book Version

---
+++ src/DirectoryEntry.java
@@ -0,0 +1,26 @@
+
+public class DirectoryEntry {
+
+ 
+	    private String name;
+	    private String number;
+	     
+	    public DirectoryEntry(String name, String phone){
+	        this.name = name;
+	        this.number = phone;
+	    }
+	    
+	    public String getNumber() {
+	    	return number;
+	    }
+	    public void setNumber(String number) {
+	    	this.number = number;
+	    }
+
+	    public String getName() {
+	    	return name;
+	    }
+	    public void setName(String name) {
+	    	this.number = name;
+	    }
+}
